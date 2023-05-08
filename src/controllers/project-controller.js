635a8893372e81080224b8302250697966d491bd
Tusho7 <sandrotushurashvili7@gmail.com>
2023-05-08 20:27:08 +0400

    feat: Create newData

--- src/controllers/project-controller.js
+++ src/controllers/project-controller.js
@@ -2,5 +2,17 @@ import project from "../models/projects.js"
 
 export const getAllProjects = async(req,res) => {
     const data = await project.find();
-    return res.json(data);
+    const newData = data.map(project => {
+        return {
+            id: project.id,
+            title: project.title,
+            description: project.description,
+            category: project.category,
+            status: project.status,
+            statusImg: project.statusImg,
+            built: project.built,
+            pricture: project.picture
+        }
+    })
+    return res.json(newData);
 }
